29f57c3a4f1070a772d720e888a3afacbfed08b6
l0l0o <loccaille@gmail.com>
2025-03-25 22:37:39 +0100

    Ajout de la gestion de l'état du minuteur avec des fonctionnalités de démarrage, d'arrêt et de réinitialisation. Intégration de SafeAreaProvider et StatusBar dans la mise en page. Mise à jour du fichier .gitignore pour inclure expo-env.d.ts.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,22 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaProvider>
+      <StatusBar style="auto" />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: {
+            flex: 1,
+          },
+          fullScreenGestureEnabled: true,
+        }}
+      >
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useRef, useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
 
 export default function Index() {
   const [counter, setCounter] = useState(25 * 60);
+  const [isRunning, setIsRunning] = useState(false);
+  const [isWorkTime, setIsWorkTime] = useState(true);
   const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
   const formatTime = (seconds: number) => {
@@ -11,7 +13,19 @@ export default function Index() {
     return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
   };
 
+  const workTime = 25 * 60;
+  const breakTime = 5 * 60;
+
+  useEffect(() => {
+    if (counter === 0) {
+      setIsWorkTime(!isWorkTime);
+      setCounter(isWorkTime ? workTime : breakTime);
+    }
+  }, [counter, isWorkTime]);
+
   const handleStart = () => {
+    setIsRunning(true);
+
     if (intervalRef.current) {
       clearInterval(intervalRef.current);
     }
@@ -25,42 +39,73 @@ export default function Index() {
     if (intervalRef.current) {
       clearInterval(intervalRef.current);
     }
+
+    setIsRunning(false);
+  };
+
+  const handleReset = () => {
+    setCounter(workTime);
   };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ fontSize: 24, fontWeight: "bold" }}>Pomopomo</Text>
-      <Text style={{ fontSize: 16, fontWeight: "bold" }}>
-        {formatTime(counter)}
-      </Text>
-      <View style={{ flexDirection: "row", gap: 10 }}>
+    <View style={styles.container}>
+      <Text style={styles.text}>Pomopomo</Text>
+      <Text style={styles.time}>{formatTime(counter)}</Text>
+      <View style={styles.buttonContainer}>
         <TouchableOpacity
-          style={{
-            backgroundColor: "blue",
-            padding: 10,
-            borderRadius: 5,
-          }}
+          style={styles.button}
           onPress={handleStart}
+          disabled={isRunning}
         >
-          <Text style={{ color: "white" }}>Start</Text>
+          <Text style={styles.buttonText}>Start</Text>
         </TouchableOpacity>
-        <TouchableOpacity
-          style={{
-            backgroundColor: "red",
-            padding: 10,
-            borderRadius: 5,
-          }}
-          onPress={handleStop}
-        >
-          <Text style={{ color: "white" }}>Stop</Text>
+        <TouchableOpacity style={styles.button} onPress={handleStop}>
+          <Text style={styles.buttonText}>Pause</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleReset}>
+          <Text style={styles.buttonText}>Reset</Text>
         </TouchableOpacity>
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+    padding: 10,
+    backgroundColor: "#FFA4AF",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#FFCCD2",
+  },
+  time: {
+    fontSize: 76,
+    fontWeight: "bold",
+    color: "#C07B84",
+  },
+  button: {
+    backgroundColor: "#FFCCD2",
+    padding: 10,
+    alignItems: "center",
+    borderRadius: 5,
+    width: "100%",
+  },
+  buttonContainer: {
+    marginTop: 20,
+    flexDirection: "column",
+    gap: 10,
+    width: "100%",
+  },
+  buttonText: {
+    color: "#C07B84",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
